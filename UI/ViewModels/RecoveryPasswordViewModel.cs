9ca67e44efadbd95c89a4b18632ff45982154815
Hân Không <hankhongg@gmail.com>
2024-12-01 19:55:30 +0700

    update recovery password feature ui + db

---
+++ UI/ViewModels/RecoveryPasswordViewModel.cs
@@ -0,0 +1,80 @@
+﻿using RestaurantManager.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RestaurantManager.ViewModels
+{
+    class RecoveryPasswordViewModel : BaseViewModel
+    {
+        private string typeInEmail;
+        public string TypeInEmail
+        {
+            get
+            {
+                return typeInEmail;
+            }
+            set
+            {
+                typeInEmail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string sentCode;
+        public string SentCode
+        {
+            get
+            {
+                return sentCode;
+            }
+            set
+            {
+                sentCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string typeInCode;
+        public string TypeInCode
+        {
+            get
+            {
+                return typeInCode;
+            }
+            set
+            {
+                typeInCode = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand ConfirmCodeCommand { get; set; }
+        public RecoveryPasswordViewModel()
+        {
+            ConfirmCodeCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                if (TypeInCode == SentCode)
+                {
+                    System.Windows.MessageBox.Show("Mã xác nhận thành công!", "Thông báo", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+                    p.Close();
+                    ProfileWindow profileWindow = new ProfileWindow();
+                    var profileVM = profileWindow.DataContext as ProfileViewModel;
+                    if (profileVM != null)
+                    {
+                        profileVM.AccountEmail = TypeInEmail;
+                        profileWindow.DataContext = profileVM;
+                        profileWindow.ShowDialog();
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Mã xác nhận không đúng!", "Thông báo", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+                }
+            });
+        }
+    }
+}
