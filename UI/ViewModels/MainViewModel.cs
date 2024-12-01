c0d2b1d645f0d8fd657c29175729fb4fb8ca25dc
Tiến Ngô <quangtienngo661@gmail.com>
2024-12-01 23:08:34 +0700

    input cus, emp, stockin db

--- UI/ViewModels/MainViewModel.cs
+++ UI/ViewModels/MainViewModel.cs
@@ -9,21 +9,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using UI.Views;
 
 namespace RestaurantManager.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
         private ObservableCollection<Customer> customerList;
+        private ObservableCollection<Employee> employeeList;
+        private ObservableCollection<Stockin> stockinList;
+        
+        private Customer newCus;
+
+        public Customer NewCus
+        {
+            get { return newCus; }
+            set 
+            { 
+                newCus = value; 
+                OnPropertyChanged();
+            }
+        }
+
         public ObservableCollection<Customer> CustomerList { get { return customerList; } set { if (customerList != value) customerList = value; OnPropertyChanged(); } }
+        public ObservableCollection<Employee> EmployeeList { get { return employeeList; } set { if (employeeList != value) employeeList = value; OnPropertyChanged(); } }
+        public ObservableCollection<Stockin> StockinList { get { return stockinList; } set { if (stockinList != value) stockinList = value; OnPropertyChanged(); } }
         public ICommand WindowIsLoadedCommand { get; set; }
         public ICommand ProfileManagementCommand {  get; set; }
         public ICommand AddOrderCommand { get; set; }
+        public ICommand AddCus { get; set; }
+        public ICommand DelCus { get; set; }
+        public ICommand ConfigCus { get; set; }
+        public ICommand SaveCus { get; set; }
 
         private string usernameForProfileWindow;
 
         public MainViewModel() {
             CustomerList = new ObservableCollection<Customer>(DataProvider.Instance.DB.Customers);
+            EmployeeList = new ObservableCollection<Employee>(DataProvider.Instance.DB.Employees);
+            StockinList = new ObservableCollection<Stockin>(DataProvider.Instance.DB.Stockins);
+
             WindowIsLoadedCommand = new RelayCommand<Window>((p) => { return true; }, (p) => 
                 { 
                     p.Hide();
@@ -58,6 +83,11 @@ namespace RestaurantManager.ViewModels
                 {
                     FoodLayoutWindow foodLayoutWindow = new FoodLayoutWindow();
                     foodLayoutWindow.ShowDialog();
+                }
+            );
+            AddCus = new RelayCommand<object>((p) => { return true; }, (p) =>
+                {
+                    
                 }
             );
         }
