c135bc0016db21b969aa4ed03697376958d3e3b8
Diễm Phạm Thị Kiều <23520286@ms.uit.edu.vn>
2024-12-07 02:11:48 +0700

    binding NewDishWindow..

--- UI/ViewModels/FoodLayoutViewModel.cs
+++ UI/ViewModels/FoodLayoutViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using RestaurantManager.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RestaurantManager.ViewModels
 {
     internal class FoodLayoutViewModel : BaseViewModel
     {
+        public ICommand NewDishCommand { get; set; }
+        public FoodLayoutViewModel()
+        {
+
+            NewDishCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                NewDishWindow wd = new NewDishWindow();
+                wd.ShowDialog();
+            });
+        }
     }
 }
