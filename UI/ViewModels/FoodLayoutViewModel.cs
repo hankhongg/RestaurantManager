83a3de4cd1d7c45cbfaf32fe565e41f9dbf49fba
Diễm Phạm Thị Kiều <23520286@ms.uit.edu.vn>
2024-12-07 02:21:21 +0700

    binding FoodItemWin..

--- UI/ViewModels/FoodLayoutViewModel.cs
+++ UI/ViewModels/FoodLayoutViewModel.cs
@@ -8,14 +8,20 @@ using System.Windows.Input;
 
 namespace RestaurantManager.ViewModels
 {
-    internal class FoodLayoutViewModel : BaseViewModel
+    class FoodLayoutViewModel : BaseViewModel
     {
         public ICommand NewDishCommand { get; set; }
+        public RelayCommand<object> FoodItemCommand { get; private set; }
+        public ICommand FoodItemComand { get; set; }
         public FoodLayoutViewModel()
         {
 
             NewDishCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
                 NewDishWindow wd = new NewDishWindow();
+                wd.ShowDialog();
+            });
+            FoodItemCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                FoodItemWindow wd = new FoodItemWindow();
                 wd.ShowDialog();
             });
         }
