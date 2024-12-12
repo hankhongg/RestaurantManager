d77893cd57f144f23055fc0d94ca52a047734d85
Diễm Phạm Thị Kiều <23520286@ms.uit.edu.vn>
2024-12-12 21:27:25 +0700

    listview ItemUC

--- UI/ViewModels/FoodLayoutViewModel.cs
+++ UI/ViewModels/FoodLayoutViewModel.cs
@@ -8,31 +8,76 @@ using System.Windows.Input;
 
 namespace RestaurantManager.ViewModels
 {
-    class FoodLayoutViewModel : BaseViewModel
+    using global::RestaurantManager.Models.DataProvider;
+    using RestaurantManager.Models.DataProvider;
+    using RestaurantManager.Views;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+
+    namespace RestaurantManager.ViewModels
     {
-        public ICommand NewDishCommand { get; set; }
-
-        public RelayCommand<object> FoodItemCommand { get; private set; }
-        public ICommand FoodItemComand { get; set; }
-
-        public ICommand EditDishComand { get; set; }
-        public RelayCommand<object> EditDishCommand { get; private set; }
-        public FoodLayoutViewModel()
+        class FoodLayoutViewModel : BaseViewModel
         {
+            public ICommand NewDishCommand { get; set; }
 
-            NewDishCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
-                NewDishWindow wd = new NewDishWindow();
-                wd.ShowDialog();
-            });
-            FoodItemCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
-                FoodItemWindow wd = new FoodItemWindow();
-                wd.ShowDialog();
-            });
-            EditDishComand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            public RelayCommand<object> FoodItemCommand { get; private set; }
+            public ICommand FoodItemComand { get; set; }
+
+            public ICommand EditDishComand { get; set; }
+            public RelayCommand<object> EditDishCommand { get; private set; }
+
+            // Danh sách các món ăn
+            private ObservableCollection<FoodItemUCViewModel> _MenuItems;
+            public ObservableCollection<FoodItemUCViewModel> MenuItems
             {
-                EditDishWindow wd = new EditDishWindow();
-                wd.ShowDialog();
-            });
+                get => _MenuItems;
+                set
+                {
+                    _MenuItems = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public void LoadMenuItems()
+            {
+                // Lấy dữ liệu từ cơ sở dữ liệu và ánh xạ vào FoodItemUCViewModel
+                var items = DataProvider.Instance.DB.MenuItems.ToList();
+                MenuItems = new ObservableCollection<FoodItemUCViewModel>(
+                    items.Select(item => new FoodItemUCViewModel
+                    {
+
+                        ItemName = item.ItemName,
+                        ItemImg = item.ItemImg,
+                        ItemSprice = item.ItemSprice
+                    })
+                );
+            }
+            public FoodLayoutViewModel()
+            {
+                LoadMenuItems();
+
+                NewDishCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                    NewDishWindow wd = new NewDishWindow();
+                    wd.ShowDialog();
+                });
+                FoodItemCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                    FoodItemWindow wd = new FoodItemWindow();
+                    wd.ShowDialog();
+                });
+                EditDishComand = new RelayCommand<object>((p) => { return true; }, (p) =>
+                {
+                    EditDishWindow wd = new EditDishWindow();
+                    wd.ShowDialog();
+                });
+
+
+            }
         }
     }
+
 }
