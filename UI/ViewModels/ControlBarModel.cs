c579b4b6c7e78c6aca9275ac5347503f5c149838
Hân Không <hankhongg@gmail.com>
2024-12-16 20:14:35 +0700

    minor update

--- UI/ViewModels/ControlBarModel.cs
+++ UI/ViewModels/ControlBarModel.cs
@@ -89,7 +89,7 @@ namespace RestaurantManager.ViewModels
                 var w = window as Window;
                 if (w!= null)
                 {
-                    if (w is MainWindow || w is ProfileWindow || w is ForgotPasswordWindow || w is RecoveryPasswordWindow)
+                    if (w is MainWindow || w is ProfileWindow || w is ForgotPasswordWindow || w is RecoveryPasswordWindow || w is AddIngreWindow || w is AddItemWindow || w is SetNameRecipeWindow)
                     {
                         Visible = Visibility.Collapsed;
                     }
